489020194dc03f7581df31d014b83cc73506e739
Sebastian B <sbustamantealfaro@gmail.com>
2022-11-16 22:29:12 -0500

    Segunda subida de archivos

---
+++ routers/webRouter.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const productosHandlebars = []
+
+const webRouter = express.Router();
+
+webRouter.get('/',(req,res) => {
+    res.render('productos', { productosHandlebars })
+})
+
+webRouter.post('/productos',(req,res) => {
+    productosHandlebars.push(req.body);
+    console.log(productosHandlebars);
+    res.redirect('/')
+})
+
+webRouter.get('/productos',(req,res) => {
+    res.render('historial', { productosHandlebars , nroProductos : productosHandlebars.length > 0 })
+})
+
+exports.webRouter = webRouter;
--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require('express');
+const {webRouter} = require('./routers/webRouter.js');
 const {routerApi} = require('./routers/routerApi.js');
+const {engine} = require('express-handlebars');
 
 const servidor = express();
 
 //middleware
 servidor.use(express.json());
 servidor.use(express.urlencoded({extended: true}));
-servidor.use('/views',express.static('views'))
+servidor.use('/formulario',express.static('public'))
+
+//servidor.use(express.static('public'));
+servidor.engine('handlebars',engine());
+servidor.set('view engine','handlebars');
+
+// servidor.get('/',(req,res) => {
+//     res.render('productos', { productos })
+// })
+
 
 //rutas
+servidor.use(webRouter);
 servidor.use('/api/productos',routerApi);
 
 const server = servidor.listen(8080, () => {
